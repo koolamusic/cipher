d8288e0668974aa9a6e2e1b46d9dff6c4b84aa93
Andrew Miracle <admin@tecmie.com>
2017-09-21 15:24:04 +0100

    complete code ccipher with jquery and custom styling

--- js/scripts.js
+++ js/scripts.js
@@ -21,11 +21,27 @@ var callTwo = function() {return initialPrompt + reverse(takeFirstLast());
 
 //create a fourth function to count the number of leters in the original sentence divide by 2 and round to a whole number
 var countLetters = function(counter) { return initialPrompt.charAt(Math.floor(counter.length/2)) + callTwo(); };
+
 /** Some secondary function to apply method to initialPrompt
 alert(countLetters(initialPrompt));  **/
 //alert(countLetters(initialPrompt));
 
-var makeReverse = alert(reverse(countLetters(initialPrompt)));
+// Make alert for function to output all exercise information
+var makeReverse = function() {
+  return reverse(countLetters(initialPrompt));
+};
 
 
 // Write jQuery function here..
+$(document).ready(function() {
+  // function for div class 1
+  $("#par1").click(function()	{
+		alert(initialPrompt);
+	});
+
+  // function for div class 2
+  $("#par2").click(function()	{
+		alert(makeReverse());
+	});
+
+});
